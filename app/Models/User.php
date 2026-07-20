9bdd618db2ad2fe8efdf1d631f63a182062edecf
muharief12 <muhammadarief379@gmail.com>
2026-07-21 01:26:51 +0700

    Updating Setup Panel Access

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
@@ -66,5 +69,15 @@ class User extends Authenticatable
     public function quranLearnings()
     {
         return $this->hasMany(QuranLearning::class, 'student_id');
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return match ($panel->getId()) {
+            'student' => true,
+            // 'dashboard' => in_array($this->role, ['admin', 'instruktur']),
+            'admin' => $this->hasRole(['Bendahara', 'Guru', 'super_admin']),
+            default => false,
+        };
     }
 }
